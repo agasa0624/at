4d9237a25412c2b56526e082a03c627a94d82d40
agasa0624 <141547619+agasa0624@users.noreply.github.com>
2023-08-30 09:51:21 +0900

    Add files via upload

--- 코딩연습/Program.cs
+++ 코딩연습/Program.cs
@@ -126,3 +126,33 @@ namespace 코딩연습
         }
 
     }
+
+
+    ///자연수 뒤집어 배열로 만들기
+    using System;
+using System.Collections.Generic;
+
+public class Solution
+    {
+        public int[] solution(long n)
+        {
+            List<int> answer = new List<int>();
+
+            while (n > 0)
+            {
+                answer.Add((int)(n % 10));
+                n /= 10;
+            }
+
+            return answer.ToArray();
+        }
+    }
+
+    //문자열을 정수로 바꾸기
+    public class Solution
+    {
+        public int solution(string s)
+        {
+            return int.Parse(s);
+        }
+    }
